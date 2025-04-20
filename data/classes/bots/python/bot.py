01bb457879292e6d1f9ad2438b057b32e5c098e6
Sean Yen (UCSD work laptop) <ussa.say@gmail.com>
2025-04-19 17:40:40 -0700

    optimize self.get_square_from_pos

--- data/classes/bots/python/bot.py
+++ data/classes/bots/python/bot.py
@@ -28,6 +28,9 @@ class FakeSquare:
     def pos(self) -> tuple[int, int]:
         return self.x, self.y
 
+    def draw(self, _):
+        pass
+
 
 class Bot:
     """
@@ -36,8 +39,6 @@ class Bot:
 
     def __init__(self):
         pass
-
-    # TODO: optmize board.get_square_from_pos(start)
 
     def _eval_board(self, side: Literal["black", "white"], board: "Board") -> float:
         score = 0
@@ -61,7 +62,9 @@ class Bot:
                     # this is KILLING an enemy pawn
                     # for black it starts at y=2 and goes up to y=5 (so from scores 1 to 4)
                     piece_score = square.y - 1 if side == "black" else 5 - square.y
-                score += piece_score * 10
+                # points for being able to kill the victim
+                score *= 1.5
+                score += piece_score * 0.5
         for square in board.squares:
             if square.occupying_piece:
                 piece_score = piece_scores[square.occupying_piece.notation]
@@ -72,11 +75,19 @@ class Bot:
                 )
         return score
 
+    def _set_board_squares(self, board: "Board", squares: list[FakeSquare]):
+        board.squares = squares
+        board._square_map = {square.pos: square for square in squares}
+
     def move(
         self, side: Literal["black", "white"], board: "Board"
     ) -> tuple[tuple[int, int], tuple[int, int]]:
         start_time = time.perf_counter()
         try:
+            # optimized version
+            board.get_square_from_pos = lambda pos: board._square_map[pos]
+            self._set_board_squares(board, board.squares)
+
             moves = board.get_all_valid_moves(side)
             pieces = {
                 (square.x, square.y): square.occupying_piece for square in board.squares
@@ -87,18 +98,20 @@ class Bot:
                 new_board = {**pieces}
                 new_board[end] = new_board[start]
                 new_board[start] = None
-                board.squares = [
-                    FakeSquare(square.x, square.y, square.occupying_piece)
-                    for square in orig_squares
-                ]
+                self._set_board_squares(
+                    board,
+                    [
+                        FakeSquare(square.x, square.y, square.occupying_piece)
+                        for square in orig_squares
+                    ],
+                )
                 start_square = board.get_square_from_pos(start)
                 end_square = board.get_square_from_pos(end)
                 if end_square.occupying_piece:
                     assert end_square.occupying_piece.color != side
                     if end_square.occupying_piece.notation == "K":
-                        # that's GOOD
-                        scores.append((start, end, math.inf))
-                        break
+                        # that's GOOD, EAT the king
+                        return start, end
                 end_square.occupying_piece = start_square.occupying_piece
                 start_square.occupying_piece = None
                 after_one_turn = board.squares
@@ -106,16 +119,19 @@ class Bot:
                 for opponent_move in board.get_all_valid_moves(
                     "white" if side == "black" else "black"
                 ):
-                    board.squares = [
-                        FakeSquare(square.x, square.y, square.occupying_piece)
-                        for square in after_one_turn
-                    ]
+                    self._set_board_squares(
+                        board,
+                        [
+                            FakeSquare(square.x, square.y, square.occupying_piece)
+                            for square in after_one_turn
+                        ],
+                    )
                     start_square = board.get_square_from_pos(opponent_move[0])
                     end_square = board.get_square_from_pos(opponent_move[1])
                     if end_square.occupying_piece:
                         assert end_square.occupying_piece.color == side
                         if end_square.occupying_piece.notation == "K":
-                            # that's bad
+                            # that's bad, they could insta kill us this way
                             worst_poss_score = -math.inf
                             break
                     end_square.occupying_piece = start_square.occupying_piece
@@ -124,7 +140,7 @@ class Bot:
                     if score < worst_poss_score:
                         worst_poss_score = score
                 scores.append((start, end, worst_poss_score))
-            board.squares = orig_squares
+            self._set_board_squares(board, orig_squares)
             # highest score first
             scores.sort(key=lambda entry: -entry[2])
             print(scores)
